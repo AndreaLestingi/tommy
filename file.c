151620e88cccd3cd95daabceb3233071fede5bfc
Andrea <lestingi13897691@luigidellerba.edu.it>
2025-02-13 10:40:13 +0100

    Update file.c

--- file.c
+++ file.c
@@ -19,15 +19,12 @@ void aggiorna() {
 void classifica() {
     int i, j; Squadra t;
     for (i = 0; i < n - 1; i++) for (j = 0; j < n - i - 1; j++) if (lega[j].punti < lega[j + 1].punti) { t = lega[j]; lega[j] = lega[j + 1]; lega[j + 1] = t; }
-    for (i = 0; i < n; i++) printf("%d. %s - %d punti
-", i + 1, lega[i].nome, lega[i].punti);
+    for (i = 0; i < n; i++) printf("%d. %s - %d punti\n", i + 1, lega[i].nome, lega[i].punti);
 }
 
 int main() {
     int s; do {
-        printf("
-1.Aggiungi 2.Aggiorna 3.Classifica 4.Esci
-Scelta: ");
+        printf("\n1.Aggiungi 2.Aggiorna 3.Classifica 4.Esci\nScelta: ");
         scanf("%d", &s);
         if (s == 1) aggiungi(); else if (s == 2) aggiorna(); else if (s == 3) classifica();
     } while (s != 4);
